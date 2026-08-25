397ae662931f079d814418773e2f3c38291d4025
Zakuan7 <harrifalfadilalah@gmail.com>
2026-08-26 02:10:52 +0800

    Create config.js from config.example.js

---
+++ config.js
@@ -0,0 +1,5 @@
+//config.js
+const SUPABASE_CONFIG = {
+    url: 'https://hvasisxszpmpswtqxmuf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2YXNpc3hzenBtcHN3dHF4bXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NjQwNzAsImV4cCI6MjEwMTE0MDA3MH0.OmJtVOrt8hMxfRmfVMJehBm5H7tpDufQM3YuVU1e0ok'
+};
